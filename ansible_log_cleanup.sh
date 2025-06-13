b3a037c0f92e62202ee2d70b3af9488421127633
Keith <8584184+git-kv@users.noreply.github.com>
2025-06-13 14:42:49 -0500

    updated script to delete log files older than 30 days

--- ansible_log_cleanup.sh
+++ ansible_log_cleanup.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 mv /home/keith/ansible_logs/ansible_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_ansible_log.txt"
+find /home/keith/ansible_logs -type f -mtime +30 -delete
