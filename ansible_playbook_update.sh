a5411df75bfb9f01fafd6bed7151764698b7bdee
Keith <8584184+git-kv@users.noreply.github.com>
2025-06-17 16:21:57 -0500

    specified non-default log file

--- ansible_playbook_update.sh
+++ ansible_playbook_update.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /home/keith/ansible
-/home/keith/.local/bin/ansible-playbook /home/keith/ansible/update.yml
+ANSIBLE_LOG_PATH=/home/keith/ansible_logs/update_log.txt /home/keith/.local/bin/ansible-playbook /home/keith/ansible/update.yml
