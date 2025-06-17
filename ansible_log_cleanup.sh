7919b96ea66d32dee2b902b4569750bc7b1e2fe1
Keith <8584184+git-kv@users.noreply.github.com>
2025-06-17 16:21:29 -0500

    added cleanup for update logs

--- ansible_log_cleanup.sh
+++ ansible_log_cleanup.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
+# default log
 mv /home/keith/ansible_logs/ansible_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_ansible_log.txt"
+
+# log for daily update playbook
+mv /home/keith/ansible_logs/update_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%y-%m-%d)_update_log.txt"
 find /home/keith/ansible_logs -type f -mtime +30 -delete
