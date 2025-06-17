e23af378aaa6c8fa1896e05332bba52df8a74eb2
Keith <8584184+git-kv@users.noreply.github.com>
2025-06-17 16:23:51 -0500

    formatted for better readability

--- ansible_log_cleanup.sh
+++ ansible_log_cleanup.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-# default log
+# backup default log
 mv /home/keith/ansible_logs/ansible_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_ansible_log.txt"
 
-# log for daily update playbook
+# backup log for daily update playbook
 mv /home/keith/ansible_logs/update_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%y-%m-%d)_update_log.txt"
+
+# delete log files older than 30 days
 find /home/keith/ansible_logs -type f -mtime +30 -delete
