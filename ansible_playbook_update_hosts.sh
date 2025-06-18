453d44d5b0cd0b41fd0cdaad63934271d22d394b
Keith <8584184+git-kv@users.noreply.github.com>
2025-06-18 09:02:48 -0500

    updated to create specific log file for updates to hosts files

--- ansible_playbook_update_hosts.sh
+++ ansible_playbook_update_hosts.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /home/keith/ansible
-/home/keith/.local/bin/ansible-playbook /home/keith/ansible/update_hosts.yml
+ANSIBLE_LOG_PATH=/home/keith/ansible_logs/update_hosts_log.txt /home/keith/.local/bin/ansible-playbook /home/keith/ansible/update_hosts.yml
