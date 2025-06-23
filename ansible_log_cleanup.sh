04e0981d050002d499bdce06632878199ed453f1
Keith <8584184+git-kv@users.noreply.github.com>
2025-06-23 14:22:17 -0500

    added cleanup of hosts file update logs

--- ansible_log_cleanup.sh
+++ ansible_log_cleanup.sh
@@ -3,7 +3,10 @@
 mv /home/keith/ansible_logs/ansible_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_ansible_log.txt"
 
 # backup log for daily update playbook
-mv /home/keith/ansible_logs/update_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%y-%m-%d)_update_log.txt"
+mv /home/keith/ansible_logs/update_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_update_log.txt"
+
+# backup log for updating host files
+mv /home/keith/ansible_logs/update_hosts_log.txt "/home/keith/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_update_hosts_log.txt"
 
 # delete log files older than 30 days
 find /home/keith/ansible_logs -type f -mtime +30 -delete
