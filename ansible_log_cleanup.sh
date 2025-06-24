5d44a837b2a1ff330a646723e1b7eb1fd8569091
Keith <8584184+git-kv@users.noreply.github.com>
2025-06-24 08:31:45 -0500

    fixed issue with file path formatting

--- ansible_log_cleanup.sh
+++ ansible_log_cleanup.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 # home default log
-mv ~/ansible_logs/ansible_log.txt "~/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_ansible_log.txt"
+cd ~/ansible_logs
+mv ansible_log.txt "$(date --date='yesterday' +%Y-%m-%d)_ansible_log.txt"
 
 # home log for daily update playbook
-mv ~/ansible_logs/update_log.txt "~/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_update_log.txt"
+mv update_log.txt "$(date --date='yesterday' +%Y-%m-%d)_update_log.txt"
 
 # home log for updating host files
-mv ~/ansible_logs/update_hosts_log.txt "~/ansible_logs/$(date --date='yesterday' +%Y-%m-%d)_update_hosts_log.txt"
+mv update_hosts_log.txt "$(date --date='yesterday' +%Y-%m-%d)_update_hosts_log.txt"
 
 # delete log files older than 30 days
 find ~/ansible_logs -type f -mtime +30 -delete
