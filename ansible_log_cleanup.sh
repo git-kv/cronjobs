8ccd83462576070f0a76ef439a9eeff85a4c4fa1
Keith <8584184+git-kv@users.noreply.github.com>
2025-06-24 13:47:04 -0500

    changed test syntax for improved compatibility between linux distributions

--- ansible_log_cleanup.sh
+++ ansible_log_cleanup.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 # home default log
 cd ~/ansible_logs
-test -f ansible_log.txt && mv ansible_log.txt "$(date --date='yesterday' +%Y-%m-%d)_ansible_log.txt"
+[ -f ansible_log.txt ] && mv ansible_log.txt "$(date --date='yesterday' +%Y-%m-%d)_ansible_log.txt"
 
 # home log for daily update playbook
-test -f update_log.txt && mv update_log.txt "$(date --date='yesterday' +%Y-%m-%d)_update_log.txt"
+[ -f update_log.txt ] && mv update_log.txt "$(date --date='yesterday' +%Y-%m-%d)_update_log.txt"
 
 # home log for updating host files
-test -f update_hosts_log.txt && mv update_hosts_log.txt "$(date --date='yesterday' +%Y-%m-%d)_update_hosts_log.txt"
+[ -f update_hosts_log.txt ] && mv update_hosts_log.txt "$(date --date='yesterday' +%Y-%m-%d)_update_hosts_log.txt"
 
 # delete log files older than 30 days
 find ~/ansible_logs -type f -mtime +30 -delete
